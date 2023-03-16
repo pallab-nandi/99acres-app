2fba7051116403ffeea2f6c8acfae97c87b33e91
pallab-nandi <pallabnandi@outlook.in>
2023-03-16 22:58:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// import express
+const express = require('express');
+
+
+// application initiate
+const app = express();
+
+
+
+
+// Server Create
+app.listen(8000, () => {
+  console.log('Application is running...');
+})
