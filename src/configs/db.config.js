016351c5c993aef807fc5cc3539ab8014afb9cd5
pallab-nandi <pallabnandi@outlook.in>
2023-04-17 19:10:43 +0530

    changes

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,4 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 module.exports = {
   dev: 'mongodb://127.0.0.1:27017/99acres-app',
-  prod: 'mongodb+srv://pallab-nandi:root@projects.v7ehl3e.mongodb.net/99acres-app'
+  prod: process.env.DB_URI
 }
